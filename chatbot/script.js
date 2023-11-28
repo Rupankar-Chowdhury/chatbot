ebdc46af8e286e649606108b623c1d38db73730e
Rupankar-Chowdhury <77725180+Rupankar-Chowdhury@users.noreply.github.com>
2023-11-28 13:55:26 +0530

    Update script.js

--- chatbot/script.js
+++ chatbot/script.js
@@ -6,6 +6,66 @@ document.addEventListener('DOMContentLoaded', function () {
     const userInput = document.getElementById('user-input');
     const sendButton = document.getElementById('send-button');
 
+    document.getElementById('minimize-button').addEventListener('click', minimizeChatbox);
+    document.getElementById('maximize-button').addEventListener('click', toggleMaximizeChatbox);
+    document.getElementById('close-button').addEventListener('click', closeChatSession);
+
+    function minimizeChatbox() {
+        const chatContainer = document.getElementById('chat-container');
+    
+        if (chatContainer.classList.contains('maximized'))
+        {
+            // If maximized, restore to the original size
+            chatContainer.classList.remove('maximized');
+            chatContainer.style.width = '';
+            chatContainer.style.height = '';
+            chatContainer.style.bottom = '80px';
+            chatContainer.style.right = '20px';
+            chatContainer.style.maxWidth = '400px';
+            chatContainer.style.borderRadius = '10px';
+        } 
+        else
+        {
+            // If not maximized, close the chatbox
+            chatContainer.style.display = 'none';
+        }
+    }
+
+    function toggleMaximizeChatbox() {
+        const chatContainer = document.getElementById('chat-container');
+        const isMaximized = chatContainer.classList.toggle('maximized');
+    
+        if (isMaximized) {
+            chatContainer.style.width = '80%'; // Adjust width as needed
+            chatContainer.style.height = '80vh'; // Adjust height as needed
+            chatContainer.style.bottom = '10px';
+            chatContainer.style.right = '10px';
+            chatContainer.style.maxWidth = 'none'; // Remove max-width for maximizing
+            chatContainer.style.borderRadius = '10px';
+        } else {
+            // Reset styles when not maximized
+            chatContainer.style.width = '';
+            chatContainer.style.height = '';
+            chatContainer.style.bottom = '80px';
+            chatContainer.style.right = '20px';
+            chatContainer.style.maxWidth = '400px';
+            chatContainer.style.borderRadius = '10px';
+        }
+    
+        // Ensure the chat messages are scrolled to the bottom when maximizing
+        chatMessages.scrollTop = chatMessages.scrollHeight;
+    }
+
+    function closeChatSession() {
+        // Clear chat history
+        chatMessages.innerHTML = '';
+        // Display initial greeting on reopening the chat
+        const welcomeMessage = `Good ${getTimeOfDay()}! I'm Insurance Genie. How may I help you?`;
+        appendMessage("Genie", welcomeMessage, "bot-message");
+        // Close chatbox
+        chatContainer.style.display = 'none';
+    }
+
     function getTimeOfDay() {
         const currentHour = new Date().getHours();
         if (currentHour >= 5 && currentHour < 12) {
@@ -63,7 +123,17 @@ document.addEventListener('DOMContentLoaded', function () {
     function appendMessage(sender, message, messageClass) {
         const messageElement = document.createElement('div');
         messageElement.classList.add('message', messageClass);
-        messageElement.innerHTML = `<strong>${sender}:</strong> ${message}`;
+    
+        if (sender === 'You') {
+            messageElement.innerHTML = `<div class="${messageClass}">${message}</div>`;
+        } else {
+            messageElement.innerHTML = `
+                <div class="${messageClass}">
+                    ${sender === 'Genie' ? '<img src="/static/img/avatar.png" alt="Bot Icon" class="bot-icon">' : ''}
+                    <div class="message-text">${message}</div>
+                </div>`;
+        }
+    
         chatMessages.appendChild(messageElement);
         chatMessages.scrollTop = chatMessages.scrollHeight;
     }
